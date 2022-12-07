59438bb95b3309121659544475eed64e9cdf371d
Milton Navarro <milton.j.navarro.p@gmail.com>
2022-12-07 09:51:10 -0600

    Post Method

--- crud-net-6/Data/Interfaces/IStudent.cs
+++ crud-net-6/Data/Interfaces/IStudent.cs
@@ -1,8 +1,13 @@
-﻿namespace crud_net_6.Data.Interfaces
+﻿using System.Linq.Expressions;
+
+namespace crud_net_6.Data.Interfaces
 {
     public interface IStudent<T>
     {
         Task<List<T>> GetAll();
         Task<T> GetById(int id);
+        Task<bool> existsAsync(Expression<Func<T, bool>> predicate);
+        Task<bool> SaveChangesAsync();
+        T add(T entity);
     }
 }
